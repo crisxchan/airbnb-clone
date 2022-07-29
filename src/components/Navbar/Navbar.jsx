542c073c2fbb431c0e4d40b9c11ac87e31949b8b
crisxchan <chrisgonzales.online@gmail.com>
2022-07-29 15:50:31 +0800

    Add UI for navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import airbnbLogo from '../../assets/airbnb-logo.png'
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnbLogo}/>
+        </nav>
+    )
+}
